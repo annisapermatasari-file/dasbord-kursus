a2f0c305403bc5cb5f397adc6438afe0776f2f41
annisapermatasari-file <annisa.permatasari@dikdasmen.belajar.id>
2026-08-15 01:04:34 +0700

    Fix Suspense import

--- app/register/page.js
+++ app/register/page.js
@@ -1,8 +1,8 @@
 'use client'
 
-import { Suspense, useSearchParams, useRouter } from 'next/navigation'
+import { Suspense, useState } from 'react'
+import { useSearchParams, useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { useState } from 'react'
 
 const plans = {
   starter: {
